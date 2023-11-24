d2199e7f7ac643e8a6dca83aa78722063ebc4247
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2023-11-24 11:48:12 +0300

    initialized the nike app store

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+
+import ProductsScreen from "./src/screens/ProductsScreen";
+import ProductsDetailsScreen from "./src/screens/ProductsDetailsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <ProductsScreen /> */}
+      <ProductsDetailsScreen />
     </View>
   );
 }
@@ -13,8 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
