2f93233d92d9b5c32447ed64f71e01a1bf35ee0a
sintayehu-B <61502710+sintayehu-B@users.noreply.github.com>
2023-11-24 11:51:18 +0300

    added productDetailedScreen

---
+++ src/screens/ProductsDetailsScreen.js
@@ -0,0 +1,90 @@
+import {
+  Button,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+  ScrollView,
+  Pressable,
+} from "react-native";
+import React from "react";
+import products from "../data/products";
+
+const ProductsDetailsScreen = () => {
+  const { width } = useWindowDimensions();
+  const product = products[0];
+
+  const addToCart = () => {
+    console.warn("add to cart");
+  };
+  return (
+    <>
+      {/* image carousel */}
+      <ScrollView>
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image
+              source={{ uri: item }}
+              style={{ width: width, aspectRatio: 1 }}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        />
+        <View style={styles.detailsContainer}>
+          <Text style={styles.title}>{product.name}</Text>
+          <Text style={styles.price}>${product.price}</Text>
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+      </ScrollView>
+      <Pressable onPress={addToCart} style={styles.button}>
+        <Text style={styles.buttonText}>Add to cart</Text>
+      </Pressable>
+      {/* navigation icon */}
+    </>
+  );
+};
+
+export default ProductsDetailsScreen;
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 18,
+  },
+  title: {
+    fontSize: 34,
+    fontWeight: "500",
+    marginVertical: 10,
+  },
+  price: {
+    fontWeight: "500",
+    fontSize: 16,
+    letterSpacing: 2,
+  },
+  description: {
+    marginVertical: 10,
+    fontSize: 18,
+    lineHeight: 30,
+    fontWeight: "300",
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "black",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    padding: 18,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+});
